b8f26f50cc46dc33e3df54c93068f2b8ed15999f
Yash _Pashte_95 <yashpashte2025.etc@mmcoe.edu.in>
2026-09-07 00:30:12 +0530

    Implement Complex number addition class

---
+++ program5.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+
+class Complex
+{
+    int real1, real2;
+    int imag1, imag2;
+
+
+public:
+    void getdata(int r1, int i1, int r2, int i2);
+
+
+    void putdata()
+    {
+        cout << "Complex number1 = " << real1 << " + " << imag1 << "i" << endl;
+        cout << "Complex number2 = " << real2 << " + " << imag2 << "i" << endl;
+
+
+        cout << "Complex Number Addition = "
+             << real1*real2-imag1*imag2<< " + "
+             << real1*imag2+imag1*real2 << "i" << endl;
+    }
+};
+
+
+void Complex::getdata(int r1, int i1, int r2, int i2)
+{
+    real1 = r1;
+    imag1 = i1;
+    real2 = r2;
+    imag2 = i2;
+}
+
+
+int main()
+{
+    Complex c;
+    int p, q, r, s;
+
+
+    cout << "Enter real part1: ";
+    cin >> p;
+
+
+    cout << "Enter imaginary part1: ";
+    cin >> q;
+
+
+    cout << "Enter real part2: ";
+    cin >> r;
+
+
+    cout << "Enter imaginary part2: ";
+    cin >> s;
+
+
+    c.getdata(p, q, r, s);
+    c.putdata();
+
+
+    return 0;
+}
